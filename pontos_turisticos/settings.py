5f1a4c27591bdc5f627f5d412e5fd29e32f8d4a8
nilton-medeiros <nilton@sistrom.com.br>
2021-01-06 09:29:20 -0300

    Correção do settings.py SECRET_KEY

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7m#)*sa0^3qyhz^2urw)^+rl7x+4$b1fk*b=$o%r^_1nv#a%_y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
