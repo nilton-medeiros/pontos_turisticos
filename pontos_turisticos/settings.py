ffd8dea972e714d8526fa845f804f80ca7d7dd88
nilton-medeiros <nilton@sistrom.com.br>
2021-01-12 15:41:22 -0300

    Configuration the app

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turisticos-ngm.herokuapp.com/', 'localhost:8000']
+ALLOWED_HOSTS = ['pontos-turisticos-ngm.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
